0d572102d8c378d0abc0cc9700bbd37c99529ec0
Sebnem <sebnemucak.a@gmail.com>
2024-10-29 00:46:37 +0300

    .

---
+++ travel-time/src/components/Line.jsx
@@ -0,0 +1,12 @@
+// Line.jsx
+const Line = () => {
+    return (
+      <div className="flex justify-center items-center w-full py-5" style={{ backgroundColor: '#c14b5f' }}>
+        <h1 className="text-center text-2xl font-semibold text-white">
+          "Gezip görmenin, yeni ufuklar açmanın en değerli yollarından biridir."
+        </h1>
+      </div>
+    );
+  };
+  
+export default Line;
---
+++ travel-time/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import Line from "../components/Line";
+
+export default function HomePage() {
+    return (
+        <div>
+            <Line />
+        </div>
+    )
+}
